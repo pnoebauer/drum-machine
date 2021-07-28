ab8a540eaaa7c08e3ea80914941933427fe3fbd3
Philip Noebauer <pnoebauer>
2021-07-28 11:53:18 +0800

    first commit - basic design implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// imported library via CDN
+// const marked = window.marked;
+
+const keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+// const clips = {Q, W, E, A, S, D, Z, X, C};
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		// console.log(this.state);
+		return (
+			<div className='container-fluid bg-dark' id='drum-machine'>
+				<div className='bg-light mx-auto p-2 mt-5' id='display'>
+					<div className='row w-50 mx-auto'>
+						<div className='col text-center bg-success p-2'>
+							<div className='px-2 m-3 bg-danger' style={{padding: '40px'}}>
+								Column
+							</div>
+						</div>
+						<div className='col text-center bg-success p-2'>
+							<div className='px-2 m-3 bg-danger' style={{padding: '40px'}}>
+								Column
+							</div>
+						</div>
+						<div className='col text-center bg-success p-2'>
+							<div className='px-2 m-3 bg-danger' style={{padding: '40px'}}>
+								Column
+							</div>
+						</div>
+						<div className='w-100'></div>
+						<div className='col text-center bg-success p-2'>
+							<div className='px-2 m-3 bg-danger' style={{padding: '40px'}}>
+								Column
+							</div>
+						</div>
+						<div className='col text-center bg-success p-2'>
+							<div className='px-2 m-3 bg-danger' style={{padding: '40px'}}>
+								Column
+							</div>
+						</div>
+						<div className='col text-center bg-success p-2'>
+							<div className='px-2 m-3 bg-danger' style={{padding: '40px'}}>
+								Column
+							</div>
+						</div>
+						<div className='w-100'></div>
+						<div className='col text-center bg-success p-2'>
+							<div className='px-2 m-3 bg-danger' style={{padding: '40px'}}>
+								Column
+							</div>
+						</div>
+						<div className='col text-center bg-success p-2'>
+							<div className='px-2 m-3 bg-danger' style={{padding: '40px'}}>
+								Column
+							</div>
+						</div>
+						<div className='col text-center bg-success p-2'>
+							<div className='px-2 m-3 bg-danger' style={{padding: '40px'}}>
+								Column
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
